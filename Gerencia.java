fcc30a5e1fa16a231a49d74fc1e8d80437289462
PedroLorea <p.lorea@edu.pucrs.br>
2022-11-25 09:57:36 -0300

    Adding mais

--- Gerencia.java
+++ Gerencia.java
@@ -1,3 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Gerencia {
+
+    public Map<String, String> programa = new HashMap<String, String>();
+    public Map<String, Integer> valores = new HashMap<String, Integer>();
     
-}
+    public Gerencia (Dados dados){
+        this.programa = dados.programa;
+        this.valores = dados.valores;
+    }
+    
+    public void worstFit(){
+
+    }
+
+    public void bestFit(){
+
+    }
+
+    public void firstFit(){
+
+    }
+
+    public void cirucularFit(){
+
+    }
+
+    public void printTela(){
+        
+    }
+}    
+
--- Main.java
+++ Main.java
@@ -1,18 +1,57 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main{
 
-    private Map<String, String> programa = new HashMap<String, String>();
-    private Map<String, Integer> valores = new HashMap<String, Integer>();
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
         
         LerArquivo lerArquivo = new LerArquivo();
 
         Dados dados = lerArquivo.Armazena("caso1.txt");
 
+        Gerencia gerencia = new Gerencia(dados);
+
+        gerencia.worstFit();
+
+
+        //======================Pra quando o programa tiver pronto========================================
+        //String arquivo = "";
+        // int op;
+
+        // System.out.println("Bem-vindo ao Gerenciador de Memória!");
+        // System.out.println("Digite o nome do arquivo:");
+        // arquivo = in.nextLine();
+
+        // LerArquivo lerArquivo = new LerArquivo();
+        // Dados dados = lerArquivo.Armazena(arquivo);
+        // Gerencia gerencia = new Gerencia(dados);
+
+        // System.out.println("Digite o número da opção desejada:");
+        // System.out.println("1- WorstFit   2- BestFit   3- FirstFit   4- CircularFit");
+        // op = in.nextInt();
         
+        // switch(op){
+        //     case 1: gerencia.worstFit();
+        //     break;
+
+        //     case 2: gerencia.bestFit();
+        //     break;
+
+        //     case 3: gerencia.firstFit();
+        //     break;
+
+        //     case 4: gerencia.cirucularFit();
+        //     break;
+
+        //     default: System.out.println("Opção inválida, tente novamente.");
+        // }
+
+        
+
+
     
     }
 }
